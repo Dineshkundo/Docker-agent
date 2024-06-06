052c73cefb8a88dc66f3197c411e7a2fb0c1bc82
Saravana <67310667+SaravanaNani@users.noreply.github.com>
2024-06-06 10:53:37 +0530

    Create main.tf

---
+++ terraform-Desk/main.tf
@@ -0,0 +1,63 @@
+provider "google" {
+  project     = "saravana95"
+  region      = "us-central1"
+}
+locals {
+  env = "desk"
+}
+
+resource "google_compute_subnetwork" "custom_subnet" {
+  name          = "${local.env}-subnet-1"
+  region        = "us-west1"  # Specify the same region as the VPC
+  network       = "jenkins-vpc"
+  ip_cidr_range = "10.0.4.0/24"  # Specify the CIDR range for your subnets
+}
+
+resource "google_compute_firewall" "allow_firewall" {
+  name    = "${local.env}-allow-8080"
+  network = "jenkins-vpc"
+  direction = "INGRESS"
+  priority = 1000
+  
+  allow {
+    protocol = "all"
+  }
+
+  source_ranges = ["0.0.0.0/0"]  # Allow traffic from any source
+  target_tags = ["desktop"]
+}
+
+
+# Create a Google Compute Engine instance
+resource "google_compute_instance" "my_instance" {
+  name         = "${local.env}"
+  machine_type = "n2-standard-2"
+  zone         = "us-west1-b"
+
+  boot_disk {
+    auto_delete = true
+
+    initialize_params {
+      image =  "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20240519"      
+      size = 20
+      type = "pd-balanced"
+    }
+  }
+
+network_interface {
+    network = "jenkins-vpc"
+    subnetwork = google_compute_subnetwork.custom_subnet.self_link
+ 
+    access_config {}
+}
+
+service_account {
+    email  = "default"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+tags = ["desktop"]
+
+metadata = {
+  ssh-keys = "root:${file("/var/lib/jenkins/.ssh/id_rsa.pub")}"
+}
